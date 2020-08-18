bdac1264713507b010f34130094aade41f0e275d
stiven c <iamalescoronado@gmail.com>
2020-08-18 16:10:26 -0500

    MEAN-CRUD

---
+++ server/index.js
@@ -0,0 +1,23 @@
+
+//iniciar el servidor en el puerto 3000
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const { mongoose } = require('./database');
+
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares 
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/users' ,require('./routes/user.routes'));
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
